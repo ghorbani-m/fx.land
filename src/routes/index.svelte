68dae4a95d3883882cd1d75b03818a4b248d92f4
ehsan shariati <ehsan6sha@gmail.com>
2022-03-21 16:44:23 -0400

    updated meta tags

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -18,7 +18,7 @@
 </script>
 
 <svelte:head>
-	<title>FunctionLand: Blockchain-Attached Storage</title>
+	<title>Functionland: Blockchain-Attached Storage</title>
 </svelte:head>
 <main>
 	<!-- <Frames /> -->
