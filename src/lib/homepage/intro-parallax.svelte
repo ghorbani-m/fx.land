3dcff8530a42b2925aab4f00757625e5b05ba714
fnfrontend <hello.farhadnawab@gmail.com>
2022-03-28 06:00:49 +0500

    intro fixed

--- src/lib/homepage/intro-parallax.svelte
+++ src/lib/homepage/intro-parallax.svelte
@@ -80,7 +80,7 @@
 <!-- <b>currentFrame {currentFrame}</b> -->
 <section> 
 	<div class="parallax-container">
-		{#if $innerWidth > 960}
+		{#if 1}
 			{#each frames as frame}
 				{#if (isNaN(currentFrame) && frame == 1) || frame === parseInt(currentFrame) }
 					<div class="frame active frame_{frame}">
